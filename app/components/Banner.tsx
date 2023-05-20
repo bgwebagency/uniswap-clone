7ba17e67c390cf04e0e62cfb21e815f7c09a29d1
kirandash <thekirankumardash@gmail.com>
2023-05-20 15:41:30 +0800

    fix: rename svg attributes

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -43,9 +43,9 @@ const Banner = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className=" ml-3.5 h-6 w-6"
           >
             <circle cx="12" cy="12" r="10"></circle>
